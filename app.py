48baaa056782cad4b7642407dc4f9f562ec9e432
MUSKAN SAINI <muskansaini0408@gmail.com>
2026-03-02 22:31:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,39 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-# Load model
+# Load trained model
 model = pickle.load(open("loan_model.pkl", "rb"))
 
 st.title("Loan Approval Prediction System")
-
 st.write("Enter Applicant Details")
 
-# Inputs (you may adjust later)
-income = st.number_input("Applicant Income")
-loan_amount = st.number_input("Loan Amount")
-credit_history = st.selectbox("Credit History (1 = Good, 0 = Bad)", [1, 0])
+# Inputs
+Gender = st.selectbox("Gender", ["Male", "Female"])
+Married = st.selectbox("Married", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["0", "1", "2", "3+"])
+Education = st.selectbox("Education", ["Graduate", "Not Graduate"])
+Self_Employed = st.selectbox("Self Employed", ["Yes", "No"])
+LoanAmount = st.number_input("Loan Amount")
+Loan_Amount_Term = st.number_input("Loan Amount Term")
+Credit_History = st.selectbox("Credit History (1=Good, 0=Bad)", [1.0, 0.0])
+Property_Area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
+Total_Income = st.number_input("Total Income")
+
+# Convert inputs to DataFrame
+input_data = pd.DataFrame({
+    'Gender': [Gender],
+    'Married': [Married],
+    'Dependents': [Dependents],
+    'Education': [Education],
+    'Self_Employed': [Self_Employed],
+    'LoanAmount': [LoanAmount],
+    'Loan_Amount_Term': [Loan_Amount_Term],
+    'Credit_History': [Credit_History],
+    'Property_Area': [Property_Area],
+    'Total_Income': [Total_Income]
+})
 
 if st.button("Predict"):
-    input_data = pd.DataFrame([[income, loan_amount, credit_history]])
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
