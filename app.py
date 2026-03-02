08e0c6e285b12bae07faf754199f96fad98117a5
MUSKAN SAINI <muskansaini0408@gmail.com>
2026-03-02 22:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-# Load trained model
+# Load model
 model = pickle.load(open("loan_model.pkl", "rb"))
 
 st.title("Loan Approval Prediction System")
 st.write("Enter Applicant Details")
 
-# Inputs
+# User Inputs
 Gender = st.selectbox("Gender", ["Male", "Female"])
 Married = st.selectbox("Married", ["Yes", "No"])
 Dependents = st.selectbox("Dependents", ["0", "1", "2", "3+"])
@@ -16,11 +16,23 @@ Education = st.selectbox("Education", ["Graduate", "Not Graduate"])
 Self_Employed = st.selectbox("Self Employed", ["Yes", "No"])
 LoanAmount = st.number_input("Loan Amount")
 Loan_Amount_Term = st.number_input("Loan Amount Term")
-Credit_History = st.selectbox("Credit History (1=Good, 0=Bad)", [1.0, 0.0])
+Credit_History = st.selectbox("Credit History", [1.0, 0.0])
 Property_Area = st.selectbox("Property Area", ["Urban", "Semiurban", "Rural"])
 Total_Income = st.number_input("Total Income")
 
-# Convert inputs to DataFrame
+# Manual Encoding (IMPORTANT)
+Gender = 1 if Gender == "Male" else 0
+Married = 1 if Married == "Yes" else 0
+Education = 1 if Education == "Graduate" else 0
+Self_Employed = 1 if Self_Employed == "Yes" else 0
+
+Dependents_map = {"0":0, "1":1, "2":2, "3+":3}
+Dependents = Dependents_map[Dependents]
+
+Property_Area_map = {"Urban":2, "Semiurban":1, "Rural":0}
+Property_Area = Property_Area_map[Property_Area]
+
+# Create DataFrame
 input_data = pd.DataFrame({
     'Gender': [Gender],
     'Married': [Married],
